5c29b51e61e8007ac181f75363e29485f9b359f3
Mike Cameron <mike@mikecameron.tv>
2018-04-16 14:52:40 -0400

    Initial Commit

---
+++ 2PlayerMath/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  2PlayerMath
+//
+//  Created by Mike Cameron on 2018-04-16.
+//  Copyright © 2018 Mike Cameron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 2PlayerMath/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  2PlayerMath
+//
+//  Created by Mike Cameron on 2018-04-16.
+//  Copyright © 2018 Mike Cameron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
